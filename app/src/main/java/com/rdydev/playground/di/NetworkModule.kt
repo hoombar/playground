a28099cdc5cbea7d2e2c3de636d4b87835420345
Ben Pearson <ben@rdydev.com>
2019-04-23 15:20:25 +0100

    add Gson for converting responses for retrofit

--- app/src/main/java/com/rdydev/playground/di/NetworkModule.kt
+++ app/src/main/java/com/rdydev/playground/di/NetworkModule.kt
@@ -5,12 +5,14 @@ import com.rdydev.playground.data.api.BlogApi
 import com.rdydev.playground.data.api.BlogService
 import org.koin.dsl.module
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 val networkModule = module {
 
     single {
         Retrofit.Builder()
             .baseUrl(BuildConfig.API_ROOT)
+            .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
 
