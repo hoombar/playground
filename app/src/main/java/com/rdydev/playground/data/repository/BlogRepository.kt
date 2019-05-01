076422e21de100c7b1044aeda5e2e0ea0e1e9732
Ben Pearson <ben@rdydev.com>
2019-05-01 09:14:20 +0100

    refactor the blog repository to remove the repeated code

--- app/src/main/java/com/rdydev/playground/data/repository/BlogRepository.kt
+++ app/src/main/java/com/rdydev/playground/data/repository/BlogRepository.kt
@@ -7,54 +7,63 @@ import com.rdydev.playground.data.dao.UserDao
 import com.rdydev.playground.data.model.domain.Comment
 import com.rdydev.playground.data.model.domain.Post
 import com.rdydev.playground.data.model.domain.User
+import io.reactivex.Completable
 import io.reactivex.Maybe
 import io.reactivex.Single
 import org.koin.core.KoinComponent
-import org.koin.core.inject
 
-class BlogRepository : KoinComponent, BlogDataProvider {
-
-    private val postDao: PostDao by inject()
-    private val commentDao: CommentDao by inject()
-    private val userDao: UserDao by inject()
-
-    private val blogApi: BlogApi by inject()
+class BlogRepository(
+    private val postDao: PostDao,
+    private val commentDao: CommentDao,
+    private val userDao: UserDao,
+    private val blogApi: BlogApi
+) : KoinComponent, BlogDataProvider {
 
     override fun getUsers(): Single<List<User>> {
-        return userDao.getAll()
-            .flatMap {
-                if (!it.isEmpty()) {
-                    Single.just(it)
-                } else {
-                    blogApi.getUsers()
-                        .map { value ->
-                            userDao.insertAll(*value.toTypedArray()).subscribe()
-                            value
-                        }
-                }
-            }
+        return fetchData(
+            local = { userDao.getAll() },
+            remote = { blogApi.getUsers() },
+            insert = { value -> userDao.insertAll(*value.toTypedArray()) }
+        )
     }
 
     override fun getComments(): Single<List<Comment>> {
-        return blogApi.getComments()
+        return fetchData(
+            local = { commentDao.getAll() },
+            remote = { blogApi.getComments() },
+            insert = { value -> commentDao.insertAll(*value.toTypedArray()) }
+        )
     }
 
     override fun getPosts(): Single<List<Post>> {
-        return postDao.getAll()
+        return fetchData(
+            local = { postDao.getAll() },
+            remote = { blogApi.getPosts() },
+            insert = { value -> postDao.insertAll(*value.toTypedArray()) }
+        )
+    }
+
+    fun getPost(postId: Int): Maybe<Post> {
+        return postDao.get(postId)
+    }
+
+    private fun <T> fetchData(
+        local: () -> Single<List<T>>,
+        remote: () -> Single<List<T>>,
+        insert: (insertValue: List<T>) -> Completable
+    ): Single<List<T>> {
+
+        return local.invoke()
             .flatMap {
-                if (!it.isEmpty()) {
+                if (it.isNotEmpty()) {
                     Single.just(it)
                 } else {
-                    blogApi.getPosts()
+                    remote.invoke()
                         .map { value ->
-                            postDao.insertAll(*value.toTypedArray()).subscribe();
+                            insert.invoke(value).subscribe();
                             value
                         }
                 }
             }
-    }
-
-    fun getPost(postId: Int) : Maybe<Post> {
-        return postDao.get(postId)
     }
 }
