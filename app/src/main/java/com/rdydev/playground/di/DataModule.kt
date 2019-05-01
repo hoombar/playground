f382f13d79bc5ebc3962de09f3b4c87200747ecb
Ben Pearson <ben@rdydev.com>
2019-05-01 09:14:06 +0100

    add tests for the BlogRepository

--- app/src/main/java/com/rdydev/playground/di/DataModule.kt
+++ app/src/main/java/com/rdydev/playground/di/DataModule.kt
@@ -6,7 +6,7 @@ import org.koin.dsl.module
 
 val dataModule = module {
 
-    single { BlogRepository() }
+    single { BlogRepository(get(), get(), get(), get()) }
 
     single { AppDatabase(get()).commentDao() }
     single { AppDatabase(get()).postDao() }
